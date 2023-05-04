e2ae52c56e297417c3372af6ccb315f3abf09ab3
lucasalbuquerques <lucasalbuquerque01@gmail.com>
2023-05-04 15:09:11 -0300

    Ajuste decimais dolar

--- Functions/dolar.js
+++ Functions/dolar.js
@@ -10,7 +10,7 @@ async function dolar(){
     try{
         var response = await axios.get('https://economia.awesomeapi.com.br/last/USD-BRL')
         const valor = parseFloat(response.data['USDBRL']['ask']);
-        valor_dolar = `O valor atual do dólar é: ${valor.toFixed(2)}`;
+        valor_dolar = `O valor atual do dólar é: ${valor.toFixed(4)}`;
         
     }catch (error) {
         valor_dolar = ['Houve um erro ao calcular o dolar, caso continue o problema favor entrar em contato com o administrador']
