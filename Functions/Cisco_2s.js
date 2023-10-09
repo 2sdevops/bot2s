66434119411c167e04e60168d39ba681d05d5941
2sdevops <120112841+2sdevops@users.noreply.github.com>
2023-10-09 17:02:16 -0300

    Update EOX and Serial number functions

--- Functions/Cisco_2s.js
+++ Functions/Cisco_2s.js
@@ -33,15 +33,26 @@ async function consultar_pn(pn_recebido) {
     json: true
     });
     resposta = response.data.EOXRecord[0]
-    retorno = (`Modelo: ${resposta.EOLProductID} \n\nEnd of Life: ${resposta.EOXExternalAnnouncementDate.value}\n\nEnd of Sale: ${resposta.EndOfSaleDate.value}\n\nEnd of Support: ${resposta.LastDateOfSupport.value}`);
-    return { success: true, message: retorno };
+    console.log(resposta)
+
+    const EOLProductID = resposta.EOLProductID;
+    const EOXExternalAnnouncementDate = resposta.EOXExternalAnnouncementDate.value;
+    const EndOfSaleDate = resposta.EndOfSaleDate.value;
+    const LastDateOfSupport = resposta.LastDateOfSupport.value;
+
+    
+
+    if (!EOLProductID || !EOXExternalAnnouncementDate || !EndOfSaleDate || !LastDateOfSupport) {
+        return { success: false, message: 'Partnumber não foi encontrado no sistema EOX da cisco, favor verificar se foi digitado corretamente' };
+    } else {
+        retorno = (`Modelo: ${EOLProductID} \n\nEnd of Life: ${EOXExternalAnnouncementDate}\n\nEnd of Sale: ${EndOfSaleDate}\n\nEnd of Support: ${LastDateOfSupport}`);
+        return { success: true, message: retorno };
+    }
     } catch (error) {
     console.error(error);
     return { success: false, message: 'Ocorreu um erro ao consultar o PN.' };
     }
     }
-
-
 
     async function consultar_sn(sn_recebido) {
         try {
@@ -54,20 +65,39 @@ async function consultar_pn(pn_recebido) {
         json: true
         });
         resposta = response.data
-        retorno = (`Tem cobertura: ${resposta.serial_numbers[0].is_covered} \n\nData final: ${resposta.serial_numbers[0].covered_product_line_end_date}\n\nNome do cliente: ${resposta.serial_numbers[0].contract_site_customer_name}`);
-        return { success: true, message: retorno };
+        console.log(resposta.serial_numbers[0].covered_product_line_end_date)
+    
+
+        const contract_site_customer_name = resposta.serial_numbers[0].contract_site_customer_name;
+        let cobertura = resposta.serial_numbers[0].is_covered;
+        
+        if (!contract_site_customer_name) {
+
+            if (cobertura === "YES") {
+                retorno = (`Tem cobertura por um contrato de serviço: Sim \n\nData final da garantia do produto: ${resposta.serial_numbers[0].warranty_end_date}\n\nNome do cliente: Não é cliente 2S`);
+                return { success: true, message: retorno };
+            } else {
+                retorno = (`Tem cobertura por um contrato de serviço: Não \n\nData final da garantia do produto: ${resposta.serial_numbers[0].warranty_end_date}\n\nNome do cliente: Não é cliente 2S`);
+                return { success: true, message: retorno };
+            }
+
+        }
+        
+
+        if (cobertura === "YES") {
+            
+            retorno = `Tem cobertura por um contrato de serviço: Sim\n\nData final do contrato de serviço: ${resposta.serial_numbers[0].covered_product_line_end_date} \n\nData final da garantia do produto: ${resposta.serial_numbers[0].warranty_end_date}\n\nNome do cliente: ${resposta.serial_numbers[0].contract_site_customer_name}`;
+            return { success: true, message: retorno };
+        } else {
+            retorno = (`Tem cobertura por um contrato de serviço: Não \n\nData final do contrato de serviço: ${resposta.serial_numbers[0].covered_product_line_end_date}\n\nNome do cliente: ${resposta.serial_numbers[0].contract_site_customer_name}`);
+            return { success: true, message: retorno };
+        }
+        
+
+        
         } catch (error) {
         console.error(error);
         return { success: false, message: 'Ocorreu um erro ao consultar o SN.' };
         }
         }
-
-
-consultar_pn('ATA190')
-
-
-
-
-
-
 
